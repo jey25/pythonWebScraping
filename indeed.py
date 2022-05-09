548301c4443aaab5ff9c18bbdef729b181ed39a8
jey25 <jjangeoyoung@gmail.com>
2022-05-09 17:50:38 +0900

    extract_indeed_jobs

--- indeed.py
+++ indeed.py
@@ -3,7 +3,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = f"https://kr.indeed.com/jobs?q=qa&limit=50&radius=25&vjk=2e794be49a16f1e7"
+LIMIT = 50
+URL = f"https://kr.indeed.com/jobs?q=qa&limit={LIMIT}&radius=25&vjk=2e794be49a16f1e7"
 
 def extract_indeed_pages():
     result = requests.get(URL)
@@ -22,3 +23,14 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_indeed_jobs(last_page):
+    jobs = []
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("a", {"class":"fs-unmask"})
+    for result in results:
+        title = result.find("h2", {"class":"jobTitle"}).find("span", title=True).string
+        company = result.find("span",{"class":"companyName"}).string
+        print(title, company)
+    return jobs
--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 
-from indeed import extract_indeed_pages
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-max_indeed_pages = extract_indeed_pages()
+last_indeed_page = extract_indeed_pages()
 
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
 
-print(max_indeed_pages)
 
 # for n in range(max_page):
 #     print(f"start={n*50}")
