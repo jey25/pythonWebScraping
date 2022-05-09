2bd5c208d98a65e3b26995b0b388bde0e3dc89b0
jey25 <jjangeoyoung@gmail.com>
2022-05-09 11:45:30 +0900

    Extracting pages

--- index.py
+++ index.py
@@ -1,9 +1,25 @@
+from pydoc import source_synopsis
+from tempfile import SpooledTemporaryFile
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://stackoverflow.com/search?q=qa")
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=qa&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&salary&radius=25&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=2e794be49a16f1e7"
 
-soup = BeautifulSoup(r.text, "html.parser")
 
-print(soup)
+indeed_result = requests.get(URL)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class":"pagination"})
+
+pages = pagination.find_all("a")
+
+spans = []
+
+for page in pages:
+    spans.append(page.find("span"))
+
+print(spans[:-1])
+
+
+
 
