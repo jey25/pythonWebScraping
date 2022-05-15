7a5b72ca3c698ab01e7bfe1a7a052cfa65a6f64b
jey25 <jjangeoyoung@gmail.com>
2022-05-15 10:17:25 +0900

    cgvmoviereviews

---
+++ CGVMovieReview.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import time
+import pandas as pd
+
+# 브라우저를 띄우지 않는 옵션
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+# C: \chromedriver\chromedriver
+
+
+def get_movie_reviews(url, page_num=10):
+
+    wd = webdriver.Chrome('chromedriver', options=chrome_options)
+    wd.get(url)
+
+    writer_list = []
+    review_list = []
+    date_list = []
+
+    for page_no in range(1, page_num+1):
+        page_ul = wd.find_element_by_id('paging_point')
+        page_a = page_ul.find_element_by_link_text(str(page_no))
+        page_a.click()
+        time.sleep(1)
+
+        writers = wd.find_elements_by_class_name('writer-name')
+        writer_list += [writer.text for writer in writers]
+
+        reviews = wd.find_elements_by_class_name('box-comment')
+        review_list += [review.text for review in reviews]
+
+        dates = wd.find_elements_by_class_name('day')
+        date_list += [date.text for date in dates]
+
+    movie_review_df = pd.DataFrame(
+        {"Writer": writer_list, "Review": review_list, "Date": date_list})
+
+    return movie_review_df
+
+
+movie_review_df = get_movie_reviews(
+    'http://www.cgv.co.kr/movies/detail-view/?midx=85715#1', 2)
+movie_review_df
